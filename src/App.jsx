275d8409541bd8de7dd1c30ab0e337e0795ce00e
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-09-09 19:59:09 -0300

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component {
+    state = {};
+    render() {
+        return (
+            <main role='main' className='container'>
+                <h1>Hello, World!</h1>
+            </main>
+        );
+    }
+}
+
+export default App;
